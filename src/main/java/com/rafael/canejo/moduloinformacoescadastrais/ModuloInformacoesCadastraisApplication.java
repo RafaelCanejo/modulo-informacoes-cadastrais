cd57ff327c1b8afdda471bc40eb58375fd91bbce
Rafael Canejo <rafael.zanella@quicko.com.br>
2022-04-15 23:36:14 -0300

    Configurando o projeto para se conectar localmente a uma instância MySQL de testes

--- src/main/java/com/rafael/canejo/moduloinformacoescadastrais/ModuloInformacoesCadastraisApplication.java
+++ src/main/java/com/rafael/canejo/moduloinformacoescadastrais/ModuloInformacoesCadastraisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ModuloInformacoesCadastraisApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "modulo-informacoes-cadastrais-config");
 		SpringApplication.run(ModuloInformacoesCadastraisApplication.class, args);
 	}
 
