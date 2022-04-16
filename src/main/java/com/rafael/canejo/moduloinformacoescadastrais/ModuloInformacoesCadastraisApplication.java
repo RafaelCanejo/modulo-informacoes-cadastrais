a513ddb01dbef135633ef56980e80ade6fc7f461
Rafael Canejo <rafael.zanella@quicko.com.br>
2022-04-15 22:02:38 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/com/rafael/canejo/moduloinformacoescadastrais/ModuloInformacoesCadastraisApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.canejo.moduloinformacoescadastrais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloInformacoesCadastraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloInformacoesCadastraisApplication.class, args);
+	}
+
+}
