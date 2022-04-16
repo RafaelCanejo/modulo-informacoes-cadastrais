cd920a6c9d4d2e441c9db3388b300dd482c07006
Rafael Canejo <rafael.zanella@quicko.com.br>
2022-04-16 01:07:21 -0300

    Documentando endpoints com o Swagger

--- src/main/java/com/rafael/canejo/moduloinformacoescadastrais/controller/CadastroAssociadoController.java
+++ src/main/java/com/rafael/canejo/moduloinformacoescadastrais/controller/CadastroAssociadoController.java
@@ -2,6 +2,7 @@ package com.rafael.canejo.moduloinformacoescadastrais.controller;
 
 import com.rafael.canejo.moduloinformacoescadastrais.dto.AssociadoDTO;
 import com.rafael.canejo.moduloinformacoescadastrais.service.AssociadoService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,29 +19,33 @@ public class CadastroAssociadoController {
   @Autowired
   private AssociadoService associadoService;
 
+  @ApiOperation(value = "Realiza uma busca de associado por seu cpf")
   @RequestMapping(method = RequestMethod.GET, produces="application/json")
   public ResponseEntity<AssociadoDTO> buscarAssociado(@RequestParam(value = "cpf") String cpf) {
     AssociadoDTO associadoDTO = associadoService.buscarAssociado(cpf);
     return ResponseEntity.ok().body(associadoDTO);
   }
 
-  @RequestMapping(method = RequestMethod.DELETE, path = "/cpf/{cpf}", produces="application/json")
+  @ApiOperation(value = "Realiza uma exclusão de associado por seu cpf")
+  @RequestMapping(method = RequestMethod.DELETE, path = "/{cpf}", produces="application/json")
   public ResponseEntity<Void> deletarAssociado(@PathVariable(value = "cpf") String cpf) {
     associadoService.deletarAssociado(cpf);
     return ResponseEntity.noContent().build();
   }
 
+  @ApiOperation(value = "Realiza uma inclusão de um novo associado")
   @RequestMapping(method = RequestMethod.POST, produces="application/json")
   public ResponseEntity<AssociadoDTO> cadastrarAssociado(@RequestBody AssociadoDTO associadoDTO) {
     AssociadoDTO associadoCadastrado = associadoService.cadastrarAssociado(associadoDTO);
-    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/cpf/{cpf}").buildAndExpand(associadoCadastrado.getCpf()).toUri();
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{cpf}").buildAndExpand(associadoCadastrado.getCpf()).toUri();
     return ResponseEntity.created(uri).body(associadoCadastrado);
   }
 
+  @ApiOperation(value = "Realiza a atualização de um associado")
   @RequestMapping(method = RequestMethod.PUT, produces="application/json")
   public ResponseEntity<AssociadoDTO> atualizarAssociado(@RequestBody AssociadoDTO associadoDTO) {
     AssociadoDTO associadoAtualizado = associadoService.atualizarAssociado(associadoDTO);
-    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/cpf/{cpf}").buildAndExpand(associadoAtualizado.getCpf()).toUri();
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{cpf}").buildAndExpand(associadoAtualizado.getCpf()).toUri();
     return ResponseEntity.created(uri).body(associadoAtualizado);
   }
 
--- src/main/java/com/rafael/canejo/moduloinformacoescadastrais/controller/CadastroPrestadorController.java
+++ src/main/java/com/rafael/canejo/moduloinformacoescadastrais/controller/CadastroPrestadorController.java
@@ -2,6 +2,7 @@ package com.rafael.canejo.moduloinformacoescadastrais.controller;
 
 import com.rafael.canejo.moduloinformacoescadastrais.dto.PrestadorDTO;
 import com.rafael.canejo.moduloinformacoescadastrais.service.PrestadorService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,29 +19,33 @@ public class CadastroPrestadorController {
   @Autowired
   private PrestadorService prestadorService;
 
+  @ApiOperation(value = "Realiza uma busca de prestador por seu registro profissional")
   @RequestMapping(method = RequestMethod.GET, produces="application/json")
   public ResponseEntity<PrestadorDTO> buscarPrestador(@RequestParam(value = "registro-profissional") String registroProfissional) {
     PrestadorDTO prestadorDTO = prestadorService.buscarPrestador(registroProfissional);
     return ResponseEntity.ok().body(prestadorDTO);
   }
 
-  @RequestMapping(method = RequestMethod.DELETE, path = "/registro-profissional/{registroProfissional}", produces="application/json")
+  @ApiOperation(value = "Realiza uma exclusão de prestador por seu registro profissional")
+  @RequestMapping(method = RequestMethod.DELETE, path = "/{registroProfissional}", produces="application/json")
   public ResponseEntity<Void> deletarPrestador(@PathVariable(value = "registroProfissional") String registroProfissional) {
     prestadorService.deletarPrestador(registroProfissional);
     return ResponseEntity.noContent().build();
   }
 
+  @ApiOperation(value = "Realiza a inclusão de um novo prestador")
   @RequestMapping(method = RequestMethod.POST, produces="application/json")
   public ResponseEntity<PrestadorDTO> cadastrarPrestador(@RequestBody PrestadorDTO prestadorDTO) {
     PrestadorDTO prestadorCadastrado = prestadorService.cadastrarPrestador(prestadorDTO);
-    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/registro-profissional/{registroProfissional}").buildAndExpand(prestadorCadastrado.getRegistroProfissional()).toUri();
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{registroProfissional}").buildAndExpand(prestadorCadastrado.getRegistroProfissional()).toUri();
     return ResponseEntity.created(uri).body(prestadorCadastrado);
   }
 
+  @ApiOperation(value = "Realiza a atualização de um prestador")
   @RequestMapping(method = RequestMethod.PUT, produces="application/json")
   public ResponseEntity<PrestadorDTO> atualizarPrestador(@RequestBody PrestadorDTO prestadorDTO) {
     PrestadorDTO prestadorAtualizado = prestadorService.atualizarPrestador(prestadorDTO);
-    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/registro-profissional/{registroProfissional}").buildAndExpand(prestadorAtualizado.getRegistroProfissional()).toUri();
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{registroProfissional}").buildAndExpand(prestadorAtualizado.getRegistroProfissional()).toUri();
     return ResponseEntity.created(uri).body(prestadorAtualizado);
   }
 
